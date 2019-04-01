b4813071cbab058c98b6455a2811ebff3f1036e6
mfpang <pangminfu89@gmail.com>
2019-04-01 22:53:49 +0800

    added validation logic to create function

--- reusable/service.go
+++ reusable/service.go
@@ -1,5 +1,10 @@
 package reusable
 
+import (
+	"errors"
+	"regexp"
+)
+
 type ProductService struct {
 	ProductRepo ProductRepository
 }
@@ -11,6 +16,22 @@ func NewService(productRepo ProductRepository) Service {
 }
 
 func (svc ProductService) Create(product *Product) (*Product, error) {
+	if product == new(Product) || product == nil {
+		return nil, errors.New("Product is empty")
+	}
+
+	if err := isProductNameValid(product.Name); err != nil {
+		return nil, err
+	}
+
+	if err := isProductDescValid(product.Desc); err != nil {
+		return nil, err
+	}
+
+	if err := isProductCodeValid(product.Code); err != nil {
+		return nil, err
+	}
+
 	return svc.ProductRepo.Create(product)
 }
 
@@ -28,4 +49,29 @@ func (svc ProductService) UpdateProduct(p *Product) (*Product, error) {
 
 func (svc ProductService) DeleteProduct(id string) error {
 	return nil
+}
+
+func isProductNameValid(name string) error {
+	if len(name) <= 3 {
+		return errors.New("Product Name must be more than 3 charater")
+	}
+
+	return nil
+}
+
+func isProductDescValid(desc string) error {
+	if len(desc) <= 10 {
+		return errors.New("Product Description must be more than 10 character")
+	}
+
+	return nil
+}
+
+func isProductCodeValid(code string) error {
+	re := regexp.MustCompile(`^[A-Z]([0-9]+)`)
+	if re.MatchString(code) {
+		return nil
+	}
+
+	return errors.New("Invalid Product code format")
 }

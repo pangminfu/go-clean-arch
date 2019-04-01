d002e75c48c816be4d3c138d54432637ba88e38d
mfpang <pangminfu89@gmail.com>
2019-04-01 22:31:49 +0800

    added positive and negative test case for create function

--- reusable/service_test.go
+++ reusable/service_test.go
@@ -3,5 +3,93 @@ package reusable
 import "testing"
 
 func TestCreate_ErrNil(t *testing.T) {
+	repo := NewInMemProductRepository(nil)
+	svc := NewService(repo)
 
+	samples := []*Product{
+		&Product{
+			Code: "A8965",
+			Name: "test",
+			Desc: "test desc",
+		},
+		&Product{
+			Code: "A0077",
+			Name: "test name",
+			Desc: "test desc not too long desc",
+		},
+		&Product{
+			Code: "A112",
+			Name: "test",
+			Desc: "test desc i dont see error",
+		},
+	}
+
+	for i, sample := range samples {
+		_, err := svc.Create(sample)
+		if err != nil {
+			t.Errorf("Sample %d should return error nil instead of %s", i+1, err.Error())
+		}
+	}
+}
+
+func TestCreate_ErrNotNil(t *testing.T) {
+	repo := NewInMemProductRepository(nil)
+	svc := NewService(repo)
+
+	samples := []*Product{
+		nil,
+		&Product{},
+		&Product{
+			Code: "8965",
+			Name: "test",
+			Desc: "test desc",
+		},
+		&Product{
+			Code: "A0077",
+			Name: "",
+			Desc: "test desc not too long desc",
+		},
+		&Product{
+			Code: "A112",
+			Name: "test",
+			Desc: "",
+		},
+		&Product{
+			Code: "112A",
+			Name: "test",
+			Desc: "",
+		},
+		&Product{
+			Code: "112",
+			Name: "test",
+			Desc: "test desc",
+		},
+		&Product{
+			Code: "A112",
+			Name: "te",
+			Desc: "test desc",
+		},
+		&Product{
+			Code: "A112",
+			Name: "test",
+			Desc: "te",
+		},
+		&Product{
+			Code: "a112",
+			Name: "test",
+			Desc: "test desc i dont see error",
+		},
+		&Product{
+			Code: "",
+			Name: "",
+			Desc: "",
+		},
+	}
+
+	for i, sample := range samples {
+		_, err := svc.Create(sample)
+		if err == nil {
+			t.Errorf("Sample %d should return error instead of error nil", i+1)
+		}
+	}
 }

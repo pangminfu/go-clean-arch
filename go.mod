e16b6cb56065ddd5b02cdac6455746baceb93077
mfpang <mfpang@sph.com.sg>
2020-02-23 16:45:23 +0800

    init unit test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pangminfu/go-clean-arch
 
 go 1.13
+
+require github.com/stretchr/testify v1.5.1
--- pkg/product/usecase/product_test.go
+++ pkg/product/usecase/product_test.go
@@ -1,7 +1,34 @@
 package usecase_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/pangminfu/go-clean-arch/pkg/product"
+	"github.com/pangminfu/go-clean-arch/pkg/product/mocks"
+	"github.com/pangminfu/go-clean-arch/pkg/product/usecase"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAdd(t *testing.T) {
+	testCases := []struct {
+		name        string
+		product     product.Product
+		expectedRes product.Product
+		expectedErr error
+	}{
+		{
+			name: "",
+		},
+	}
 
+	for _, testCase := range testCases {
+		repo := new(mocks.Repository)
+
+		uc := usecase.New(repo)
+
+		result, err := uc.Add(testCase.product)
+
+		assert.Equal(t, testCase.expectedErr, err)
+		assert.Equal(t, testCase.expectedRes, res)
+	}
 }

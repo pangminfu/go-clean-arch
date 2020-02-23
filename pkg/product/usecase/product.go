8feeb486b77f25167efc0c07dec4db3eb297fb93
mfpang <mfpang@sph.com.sg>
2020-02-23 16:21:47 +0800

    usecase no implementation

---
+++ pkg/product/usecase/product.go
@@ -0,0 +1,37 @@
+package usecase
+
+import (
+	"errors"
+
+	"github.com/pangminfu/go-clean-arch/pkg/product"
+)
+
+type useCaseImpl struct {
+	repo product.Repository
+}
+
+func New(repo product.Repository) product.Usecase {
+	return &useCaseImpl{
+		repo: repo,
+	}
+}
+
+func (uc *useCaseImpl) Add(p *product.Product) (*product.Product, error) {
+	return nil, errors.New("no implementation")
+}
+
+func (uc *useCaseImpl) Products() ([]product.Product, error) {
+	return nil, errors.New("no implementation")
+}
+
+func (uc *useCaseImpl) Search(code string) (*product.Product, error) {
+	return nil, errors.New("no implementation")
+}
+
+func (uc *useCaseImpl) Update(p *product.Product) (*product.Product, error) {
+	return nil, errors.New("no implementation")
+}
+
+func (uc *useCaseImpl) Delete(id int) error {
+	return errors.New("no implementation")
+}

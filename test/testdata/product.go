9f0f45c8aca5498563771abc6eb88e7ffd195293
mfpang <mfpang@sph.com.sg>
2020-02-23 21:00:35 +0800

    testdata for product

---
+++ test/testdata/product.go
@@ -0,0 +1,11 @@
+package testdata
+
+import "github.com/pangminfu/go-clean-arch/pkg/product"
+
+var (
+	ValidProduct = product.Product{
+		Code: "SG001",
+		Name: "Macbook",
+		Desc: "This is a macbook",
+	}
+)

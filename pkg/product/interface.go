c2ee8746bad134f4f894e25ac713caf93ac369a1
mfpang <mfpang@sph.com.sg>
2020-02-23 15:59:09 +0800

    new interface

--- pkg/product/interface.go
+++ pkg/product/interface.go
@@ -1,17 +1,17 @@
 package product
 
-type Service interface {
-	Create(p *Product) (*Product, error)
-	ListProduct() ([]*Product, error)
-	SearchByCode(code string) (*Product, error)
-	UpdateProduct(p *Product) (*Product, error)
-	DeleteProduct(id int) error
+type Usecase interface {
+	Add(p *Product) (*Product, error)
+	Products() ([]*Product, error)
+	Search(code string) (*Product, error)
+	Update(p *Product) (*Product, error)
+	Delete(id int) error
 }
 
-type ProductRepository interface {
-	Create(p *Product) (*Product, error)
-	List() ([]*Product, error)
-	GetByCode(code string) (*Product, error)
+type Repository interface {
+	Add(p *Product) (*Product, error)
+	Products() ([]*Product, error)
+	Search(code string) (*Product, error)
 	Update(p *Product) (*Product, error)
 	Delete(id int) error
 }

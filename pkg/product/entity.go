3b7daf959dbcb03e8ba31b9a1502bf874fa87721
mfpang <mfpang@sph.com.sg>
2020-02-23 21:30:28 +0800

    product code validation

--- pkg/product/entity.go
+++ pkg/product/entity.go
@@ -1,8 +1,23 @@
 package product
 
+import (
+	"errors"
+	"regexp"
+)
+
 type Product struct {
-	Id   int
+	ID   int
 	Code string
 	Name string
 	Desc string
+}
+
+func (p Product) IsCodeValid() error {
+	re := regexp.MustCompile(`^[A-Z]{3}[0-9]{4}$`)
+	isValid := re.MatchString(p.Code)
+	if !isValid {
+		return errors.New("invalid product code")
+	}
+
+	return nil
 }

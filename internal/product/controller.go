ac024b867881659fce3afb9dc2cc7761089786e2
mfpang <mfpang@sph.com.sg>
2020-02-23 22:29:55 +0800

    controller implementation

--- internal/product/controller.go
+++ internal/product/controller.go
@@ -1,24 +1,66 @@
 package product
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
-	productusecase "github.com/pangminfu/go-clean-arch/pkg/product"
+	productservice "github.com/pangminfu/go-clean-arch/pkg/product"
 )
 
 type Controller struct {
-	uc productusecase.Usecase
+	uc productservice.Usecase
 }
 
-func New(uc productusecase.Usecase) *Controller {
+func New(uc productservice.Usecase) *Controller {
 	return &Controller{
 		uc: uc,
 	}
 }
 
 func (ctrl *Controller) PostProduct(c *gin.Context) {
+	product := new(productservice.Product)
+	if err := c.ShouldBindJSON(&product); err != nil {
+		errResponse(c, err, http.StatusBadRequest)
 
+		return
+	}
+
+	result, err := ctrl.uc.Add(*product)
+	if err != nil {
+		errResponse(c, err, http.StatusInternalServerError)
+
+		return
+	}
+
+	c.JSON(
+		http.StatusCreated,
+		gin.H{
+			"data": result,
+		},
+	)
 }
 
 func (ctrl *Controller) GetProducts(c *gin.Context) {
+	result, err := ctrl.uc.Products()
+	if err != nil {
+		errResponse(c, err, http.StatusInternalServerError)
 
+		return
+	}
+
+	c.JSON(
+		http.StatusOK,
+		gin.H{
+			"data": result,
+		},
+	)
+}
+
+func errResponse(c *gin.Context, err error, status int) {
+	c.JSON(
+		status,
+		gin.H{
+			"error": err.Error(),
+		},
+	)
 }

473d7c3371b45768b48de5609891ba2b9c516fc5
mfpang <mfpang@sph.com.sg>
2020-03-01 23:06:06 +0800

    route init with mysql db

--- internal/router/route.go
+++ internal/router/route.go
@@ -3,17 +3,19 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	productctrl "github.com/pangminfu/go-clean-arch/internal/product"
-	"github.com/pangminfu/go-clean-arch/pkg/product"
 	"github.com/pangminfu/go-clean-arch/pkg/product/repository"
 	"github.com/pangminfu/go-clean-arch/pkg/product/usecase"
 )
 
-var db []product.Product
-
 func Init() (*gin.Engine, error) {
 	r := gin.Default()
 
-	repo := repository.New(db)
+	db, err := repository.ConnectMySQL("root", "root", "127.0.0.1", "3306", "product_db")
+	if err != nil {
+		return nil, err
+	}
+
+	repo := repository.NewMysql(db)
 	uc := usecase.New(repo)
 	ctrl := productctrl.New(uc)
 

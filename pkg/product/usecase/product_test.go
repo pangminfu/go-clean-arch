53a1d8d4c094903c75eb446dc435c94bede02d8c
mfpang <mfpang@sph.com.sg>
2020-03-02 22:18:51 +0800

    update test case

--- pkg/product/usecase/product_test.go
+++ pkg/product/usecase/product_test.go
@@ -131,3 +131,45 @@ func TestSearch(t *testing.T) {
 		assert.Equal(t, testCase.expectedRes, result)
 	}
 }
+
+func TestUpdate(t *testing.T) {
+	testCases := []struct {
+		name        string
+		product     product.Product
+		repoRes     product.Product
+		repoErr     error
+		expectedRes product.Product
+		expectedErr error
+	}{
+		{
+			name:        "Given valid product product detail",
+			product:     testdata.ProductValid,
+			repoRes:     testdata.ProductValid,
+			repoErr:     nil,
+			expectedRes: testdata.ProductValid,
+			expectedErr: nil,
+		},
+		{
+			name:        "Given repo return error",
+			product:     testdata.ProductValid,
+			repoRes:     testdata.ProductZeroValue,
+			repoErr:     testdata.RepoErr,
+			expectedRes: testdata.ProductZeroValue,
+			expectedErr: testdata.RepoErr,
+		},
+	}
+
+	for _, testCase := range testCases {
+		repo := new(mocks.Repository)
+
+		repo.On("Update", testCase.product).
+			Return(testCase.repoRes, testCase.repoErr)
+
+		uc := usecase.New(repo)
+
+		result, err := uc.Update(testCase.product)
+
+		assert.Equal(t, testCase.expectedErr, err)
+		assert.Equal(t, testCase.expectedRes, result)
+	}
+}
